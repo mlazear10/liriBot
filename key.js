2ce860b722bca55a949e02f92343213639dbb517
mlazear10 <mlazear10@icloud.com>
2017-09-06 17:23:04 -0700

    Updated

--- key.js
+++ key.js
@@ -4,6 +4,10 @@ exports.twitterKeys = {
   consumer_key: '5TtRbRflpF4BXKTjUgQB2sP64',
   consumer_secret: 'POuEeoi4bgbdRBCwPCU7Kie5YhR5QtsWMcNl9MksQAEWZIf45U',
   access_token_key: '900428477966426112-H67BJfxYwE3LNzmiHI0zxbc5pXQxWm7',
-  access_token_secret: 'uYptFXjcS0pd1EuD4C5MYSigr3AvO51er0DHwXgLKA5EI
-  ',
+  access_token_secret: 'uYptFXjcS0pd1EuD4C5MYSigr3AvO51er0DHwXgLKA5EI',
+}
+
+exports.spotify = {
+  Client_ID: '66305cbe81c14632b276edd5e6780b7e',
+  Client_Secret: '0e1ee723343e4fbe9790a29429d86776',
 }
--- liri.js
+++ liri.js
@@ -1,3 +1,48 @@
-var require = require("require");
+var keys = require("./key.js");
+var Twitter = require('twitter');
+var Spotify = require('node-spotify-api');
 
-require(key.js);
+var getMyTweets = function() {
+    var client = new Twitter(keys.twitterKeys);
+
+    var params = {screen_name: 'inrtracker'}
+    client.get('statuses/user_timeline', params, function(error, tweets, response){ 
+    if(!error) {
+        for (var i = 0; i < tweets.length; i++) {
+            console.log(tweets[i].created_at);
+            console.log(' ');
+            console.log(tweets[i].text);
+        } 
+    }
+    });
+}
+
+
+var spotify = new Spotify({
+ id: keys.spotify.Client_ID,
+ secret: keys.spotify.Client_Secret,
+});
+
+spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
+ if (err) {
+   return console.log('Error occurred: ' + err);
+ }
+
+console.log(data); 
+});
+
+
+var pick = function(caseData, functionData) {
+    switch(caseData) {
+        case 'my-tweets' : getMyTweets();
+        break;
+        default:
+        console.log('LIRI does not know that');
+    }
+}
+
+var runThis = function(argOne, argTwo) {
+    pick(argOne, argTwo);
+};
+
+runThis(process.argv[2], process.argv[3]);
