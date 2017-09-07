206936e2a94673c7ffd3b85df9c6aa5d31b73f75
mlazear10 <mlazear10@icloud.com>
2017-09-06 17:58:49 -0700

    updates

--- liri.js
+++ liri.js
@@ -1,6 +1,7 @@
 var keys = require("./key.js");
 var Twitter = require('twitter');
 var Spotify = require('node-spotify-api');
+var request = require('request');
 
 var getMyTweets = function() {
     var client = new Twitter(keys.twitterKeys);
@@ -17,24 +18,47 @@ var getMyTweets = function() {
     });
 }
 
+var getArtistNames = function(artist) {
+    return artist.name;
+}
 
-var spotify = new Spotify({
- id: keys.spotify.Client_ID,
- secret: keys.spotify.Client_Secret,
+var songChoice = function(songName) {
+    var spotify = new Spotify({
+        id: keys.spotify.Client_ID,
+        secret: keys.spotify.Client_Secret,
+    });
+
+    spotify.search({ type: 'track', query: songName, limit: 20 }, function(err, data) {
+        if (err) {
+          return console.log('Error occurred: ' + err);
+        }
+        var songs = data.tracks.items;
+        for (var i = 0; i < songs.length; i++) {
+          console.log(i + 1);
+          console.log('Artist/Band: ' + songs[i].artists.map(getArtistNames));
+          console.log('Song Title: ' + songs[i].name);
+          console.log('Preview URL: ' + songs[i].preview_url);
+          console.log('Album Name: ' + songs[i].album.name);
+          console.log('-------------------------------------');
+        }
+       });
+};
+
+function omdbThisMovie(movieTitle) {
+
+    request('http://www.omdbapi.com/?t=' + movieTitle + '&y=&plot=short&apikey=409cece', function (error, response, body) {
+      if(error) {
+          ////////////
+      }
 });
-
-spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
- if (err) {
-   return console.log('Error occurred: ' + err);
- }
-
-console.log(data); 
-});
+}
 
 
 var pick = function(caseData, functionData) {
     switch(caseData) {
         case 'my-tweets' : getMyTweets();
+        break;
+        case 'spotify-this-song': songChoice(functionData);
         break;
         default:
         console.log('LIRI does not know that');
